2c06e5a1be4af5274418850433220c3f96ba4905
Eric <Aerelioyanadel@gmail.com>
2020-07-07 16:33:02 -0400

    navbar creation

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react"
+
+import "./style.css"
+
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => setIsOpen(!isOpen)
+
+  return (
+    <nav>
+      <div className="logo">
+        <h4>Logo</h4>
+      </div>
+      <ul className={isOpen ? "nav-links nav-active" : "nav-links"}>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">blog</a>
+        </li>
+      </ul>
+      <div className={isOpen ? "burger open" : "burger"} onClick={toggle}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
