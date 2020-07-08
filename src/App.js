27beac072ef443d9a60547c120f770d913de590a
Eric <Aerelioyanadel@gmail.com>
2020-07-08 19:09:56 -0400

    update collections

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react"
 import NavBar from "./components/Navbar"
-// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 
 function App() {
   return (
     <div>
-      <NavBar></NavBar>
+      <NavBar />
     </div>
   )
 }
